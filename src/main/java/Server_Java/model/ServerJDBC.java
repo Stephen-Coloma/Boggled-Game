576ec8e1beac882b2cddfedb24b7c701b5cde6c0
MARIUS GLENN NONATO <2232731@slu.edu.ph>
2024-04-22 22:55:00 +0800

    feat(server): Implemented logging in and logging out statements for the user
    >This commit adds the logic for logging in and logging out of the user

--- src/main/java/Server_Java/model/ServerJDBC.java
+++ src/main/java/Server_Java/model/ServerJDBC.java
@@ -11,6 +11,9 @@ public class ServerJDBC {
     private static final String USER = "user";
     private static final String PASSWORD = "password";
     private static Connection connection;
+    private static String query;
+    private static PreparedStatement preparedStatement;
+    private static ResultSet resultSet;
 
     static {
         try {
@@ -21,13 +24,49 @@ public class ServerJDBC {
     }
 
     public static Player login(String username, String password) throws AccountDoesNotExist, AlreadyLoggedIn {
-        //todo: database queries for login
+        query = "SELECT * FROM players WHERE Username = ? AND Password = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String fn = resultSet.getString("fullname");
+                int points = resultSet.getInt("points");
+                int loggedInStatus = resultSet.getInt("loggedinstatus");
+                if (loggedInStatus == 1) {
+                    throw new AlreadyLoggedIn("Account already logged in");
+                } else {
+                    return new Player(id, fn, username, password, points, 0);
+                }
+            } else {
+                throw new AccountDoesNotExist("Account Does Not Exist");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     public static void logout(int pid) {
-        //todo: database queries for logout
+        query = "UPDATE players SET loggedinstatus = 0 WHERE pid = ?";
+        try{
+            preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, pid);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+
     }
+
 
     public static int getLastGameId() {
         //todo: get the latest game id from the games table in boggleddb database, return 0 if there is non yet.
